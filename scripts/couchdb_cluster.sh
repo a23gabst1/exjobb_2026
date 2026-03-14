70da4ea429b1626ca0a84c937f66d78e092b6afd
a23gabst1 <a23gabst@student.his.se>
2026-03-14 14:52:34 +0100

    script: initializes a four node couchdb cluster by enabling it on the primary node which other nodes can later join

--- scripts/couchdb_cluster.sh
+++ scripts/couchdb_cluster.sh
@@ -9,3 +9,19 @@ SECONDARIES="1 2 3"
 docker compose -f ../docker/docker-compose-couchdb.yaml up -d
 
 sleep 5
+
+# Initializes a cluster by enabling it and it will consist of four nodes (node_count)
+# It could respond with a bad_request, and it means that the cluster has already been enabled
+# This setup request is sent to the primary node
+for NODE_ID in $PRIMARY
+do
+    curl -X POST "http://user:password@localhost:${BASE_PORT}${NODE_ID}/_cluster_setup" \
+    -H "Content-Type: application/json" \
+    -d '{
+        "action": "enable_cluster",
+        "bind_address": "0.0.0.0",
+        "username": "user",
+        "password": "password", 
+        "node_count": "4"
+    }'
+done
