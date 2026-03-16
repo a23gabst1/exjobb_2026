4573166161c25bfbaec563d037f2201aceaf64c5
a23gabst1 <a23gabst@student.his.se>
2026-03-16 13:51:51 +0100

    Added an endpoint that initializes the experiment and creates a csv file which stores the results

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ import { fileURLToPath } from "node:url"
 import path from "node:path"
 import { mongoRouter } from "./routes/mongodb_router.js"
 import { couchRouter } from "./routes/couchdb_router.js"
+import { writeFile } from "node:fs/promises"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
+
+const docSizes = {
+    "9": "1M",
+    "49": "5M",
+    "99": "10M"
+};
+
+const numOfDocuments = docSizes[process.env.DOC_SIZE];
+let selectedDatabase = null;
 
 // Express middlewares
 app.use(express.json()); // Only parses JSON
@@ -20,6 +30,29 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Endpoint that initializes the experiment
+// Creates the file to store the results
+app.get("/init_experiment", async (req, res) => {
+    const { database } = req.query;
+    selectedDatabase = database;
+
+    try {
+        const csvHeader = "old,new,delta,patient_id";
+        const resultFolder = "measures";
+        const fullPath = path.join(__dirname, resultFolder, `${numOfDocuments}_${selectedDatabase}.csv`);
+        await writeFile(fullPath, csvHeader);
+
+        return res.status(200).json({
+            msg: "Start the experiment",
+            start: true
+        });
+    } catch (error) {
+        console.error("Error when creating the file (init_experiment - endpoint)", error);
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Running on http://localhost:${PORT}`);
 });
+
+export { numOfDocuments, selectedDatabase };
