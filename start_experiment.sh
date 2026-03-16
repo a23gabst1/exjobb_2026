c9083a0319b1ae3fa40aa012d636b856c80f891b
a23gabst1 <a23gabst@student.his.se>
2026-03-16 12:49:00 +0100

    Added a last script which creates the data, starts the clusters and lastly starts the whole application

---
+++ start_experiment.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+IMAGE_COUNT=$1
+
+cd scripts/
+
+chmod +x ./generate_data.sh ./mongodb_cluster.sh ./couchdb_cluster.sh
+
+./generate_data.sh $IMAGE_COUNT
+
+./mongodb_cluster.sh
+
+./couchdb_cluster.sh $IMAGE_COUNT
+
+cd ..
+
+DOC_SIZE=$IMAGE_COUNT node index.js
+
+sleep 3
+
+find ./chunks -type f -iname "chunk_*" | xargs rm -rv
