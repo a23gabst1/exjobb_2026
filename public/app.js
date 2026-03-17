f4f2aaa10d6eb36b951260b6575134aa0d5b282f
a23gabst1 <a23gabst@student.his.se>
2026-03-17 15:49:06 +0100

    Added two buttons to choose which database to start measuring, Added more styling to the buttons and lastly an variable stores the selected database

--- public/app.js
+++ public/app.js
@@ -1,5 +1,32 @@
+/**
+ * Function that is responsible for switching pages
+ * 
+ * @param {number} page 
+ */
 function slideToPage(page) {
     const tracker = document.querySelector(".slider_tracker");
 
     tracker.style.transform = `translateX(-${page * 100}%)`;
 }
+
+const page1 = document.getElementById("page_step_0");
+let selectedDatabase = null;
+
+/**
+ * Function that is the handler of all click events on the page
+ * 
+ * @param {Event} event 
+ */
+function handlePage1Click(event) {
+    const element = event.target;
+
+    if (!element.classList.contains("mongodb") && !element.classList.contains("couchdb")) {
+        console.log("Nope");
+        return;
+    }
+
+    selectedDatabase = element.classList.contains("mongodb") ? "MongoDB" : "CouchDB";
+    slideToPage(1);
+}
+
+page1.addEventListener("click", handlePage1Click);
