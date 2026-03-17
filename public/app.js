f1c6d5a37599356474f2e00dffa7e207aad3b049
a23gabst1 <a23gabst@student.his.se>
2026-03-17 16:35:10 +0100

    Added a event handler to the start button and it checks that the trial input is not under or equal, or empty

--- public/app.js
+++ public/app.js
@@ -30,3 +30,28 @@ function handlePage1Click(event) {
 }
 
 page1.addEventListener("click", handlePage1Click);
+
+const startExperimentBtn = document.querySelector("#page_step_1 input[type='button']");
+
+let experimentTrials = null;
+/**
+ * Function that handles all click events on the start button
+ * 
+ * @param {Event} event 
+ */
+function handleStartBtnClick(event) {
+    const trialInput = event.currentTarget.previousElementSibling;
+    const numOfTrials = parseInt(trialInput.value.trim());
+    if (numOfTrials < 0 || numOfTrials === 0 || trialInput.value === "") {
+        alert("Trials cannot be under or equal to 0, or empty!");
+        return;
+    }
+
+    experimentTrials = numOfTrials;
+}
+
+startExperimentBtn.addEventListener("click", handleStartBtnClick);
+
+(function () {
+    slideToPage(1);
+})();
