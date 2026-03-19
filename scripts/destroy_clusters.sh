5b602c385c7ce32ca3b77b0651822ff04c4ab00e
a23gabst1 <a23gabst@student.his.se>
2026-03-19 14:17:31 +0100

    Added a if-else statement to make sure that it destroy all the services that are currently up and running

--- scripts/destroy_clusters.sh
+++ scripts/destroy_clusters.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+DB=$1
+
 echo "Destroying services..."
 
 # Destroys all the services set up in the compose files
-docker compose -f docker/docker-compose-couchdb.yaml down -v
+if [ "${DB}" = "couchdb" ]; then
+    docker compose -f docker/docker-compose-couchdb.yaml down -v
+else
+    docker compose -f docker/docker-compose-mongo-config.yaml down
 
-docker compose -f docker/docker-compose-mongo-config.yaml down
+    docker compose -f docker/docker-compose-mongo-shards.yaml down
 
-docker compose -f docker/docker-compose-mongo-shards.yaml down
-
-docker compose -f docker/docker-compose-mongo-mongos.yaml down
+    docker compose -f docker/docker-compose-mongo-mongos.yaml down
+fi
 
 echo "Done!"
