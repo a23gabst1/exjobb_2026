2b0321da333e9872d23f07e991319a5ee53139b8
a23gabst1 <a23gabst@student.his.se>
2026-03-19 14:19:54 +0100

    Fixed: Only one of the database clusters can be up and running to make sure that he memory limit and CPU usage is not greater than the limits on Docker Desktop

--- start_experiment.sh
+++ start_experiment.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 IMAGE_COUNT=$1
+DATABASE=$2
+
+# Exits the process if one of the necessary arguments are empty
+if [ -z "${IMAGE_COUNT}" ] || [ -z "${DATABASE}" ]; then
+    echo "Image count or database arguments cannot be empty"
+    exit 1
+fi
+
+# Exits the process if the users did not type in one of the databases below (exactly with lowercase)
+if [ "${DATABASE}" != "mongodb" ] && [ "${DATABASE}" != "couchdb" ]; then
+    echo "'mongodb' or 'couchdb' (exact)"
+    exit 1
+fi
 
 cd scripts/
 
@@ -8,13 +21,16 @@ chmod +x ./generate_data.sh ./mongodb_cluster.sh ./couchdb_cluster.sh
 
 ./generate_data.sh $IMAGE_COUNT
 
-./mongodb_cluster.sh
-
-./couchdb_cluster.sh $IMAGE_COUNT
+# Only one of the databases can run since it goes over the memory and CPU usage limit on higher number of documents on Docker Desktop
+if [ "$DATABASE" = "mongodb" ]; then
+    ./mongodb_cluster.sh
+else 
+    ./couchdb_cluster.sh $IMAGE_COUNT
+fi
 
 cd ..
 
-DOC_SIZE=$IMAGE_COUNT node server.js
+DOC_SIZE=$IMAGE_COUNT DB=$DATABASE node server.js
 
 sleep 3
 
