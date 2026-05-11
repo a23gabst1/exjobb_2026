b8893aada9b1861053faf633f9f19e5c83d087c8
a23gabst1 <a23gabst@student.his.se>
2026-05-11 18:41:21 +0200

    Client Javascript (Measurement script)
    Added another request function solely for the image experiment and changed the code to make sure that it runs based on the decisions the client takes on the UI.

--- public/app.js
+++ public/app.js
@@ -4,6 +4,7 @@ const maxNumOfPatients = {
     "10": 767899
 };
 let docSize = null;
+let imgExperiment = null;
 
 /**
  * Function that connects to an event stream from the server to know which database cluster is configured and ready to be measured
@@ -71,7 +72,7 @@ let experimentTrials = null;
  * @param {Event} event 
  */
 async function handleStartBtnClick(event) {
-    const trialInput = event.currentTarget.previousElementSibling;
+    const trialInput = event.currentTarget.previousElementSibling.previousElementSibling;
     const numOfTrials = parseInt(trialInput.value.trim());
 
     if (numOfTrials < 0 || numOfTrials === 0 || trialInput.value === "") {
@@ -86,7 +87,12 @@ async function handleStartBtnClick(event) {
     if (start) {
         slideToPage(2);
         setTimeout(async () => {
-            await sendRequest();
+            if (imgExperiment == null) {
+                await sendRequest();
+            }
+            else {
+                await sendRequest2();
+            }
         }, 4000);
     }
 }
@@ -116,6 +122,18 @@ async function initializeExperiment() {
         console.error("Error when trying to initialize experiment", error);
     }
 }
+
+const imgBtnWrapper = document.querySelector(".img_btn_wrapper");
+
+imgBtnWrapper.addEventListener("click", (event) => {
+    const element = event.target;
+    if (!element.classList.contains("img_l") && !element.classList.contains("img_s")) {
+        return;
+    }
+
+    const dataset = element.dataset.img_type;
+    imgExperiment = dataset;
+});
 
 startExperimentBtn.addEventListener("click", handleStartBtnClick);
 
@@ -158,11 +176,77 @@ async function sendRequest(currentIteration = 0) {
 
         createView(data, patientID);
 
-        setTimeout(async () => {
-            await sendRequest(currentIteration + 1);
-        }, 2000);
+        // setTimeout(async () => {
+        //     await sendRequest(currentIteration + 1);
+        // }, 2000);
+        await sendRequest(currentIteration + 1);
     } catch (error) {
         console.error(`Error when sending request to '/${database}' endpoint`, error);
+    }
+}
+
+async function sendRequest2(currentIteration = 0) {
+    if (currentIteration >= experimentTrials) {
+        alert("Experiment is done");
+        slideToPage(0);
+        imgExperiment = null;
+        return;
+    }
+
+    Math.setSeed(currentIteration);
+    let patientID;
+    try {
+        switch (imgExperiment) {
+            case "L": {
+                let patientIdx;
+
+                if (docSize === "10") {
+                    patientIdx = Math.floor(Math.random() * patient_ids_10_l.length);
+                    patientID = patient_ids_10_l[patientIdx];
+                }
+
+                if (docSize === "5") {
+                    patientIdx = Math.floor(Math.random() * patient_ids_5_l.length);
+                    patientID = patient_ids_5_l[patientIdx];
+                }
+
+                break;
+            }
+
+            case "S": {
+                let patientIdx;
+                if (docSize === "10") {
+                    patientIdx = Math.floor(Math.random() * patient_ids_10_s.length);
+                    patientID = patient_ids_10_s[patientIdx];
+                }
+                if (docSize === "5") {
+                    patientIdx = Math.floor(Math.random() * patient_ids_5_s.length);
+                    patientID = patient_ids_5_s[patientIdx];
+                }
+                break;
+            }
+
+            default:
+                break;
+        }
+
+        const database = selectedDatabase.toLowerCase();
+        const url = `http://localhost:3000/${database}/${patientID}`;
+
+        const response = await fetch(url, {
+            method: "GET"
+        });
+
+        const data = await response.json();
+
+        createView(data, patientID);
+
+        // setTimeout(async () => {
+        //     await sendRequest2(currentIteration + 1);
+        // }, 2000);
+        await sendRequest2(currentIteration + 1);
+    } catch (error) {
+        console.error("Error on img experiment", error);
     }
 }
 
